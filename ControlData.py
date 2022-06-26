be1405147e1dda85895fb283bd5330f06e5ea64c
bhoeflich <hoed>
2022-06-26 18:29:35 +0200

    path fixing

--- ControlData.py
+++ ControlData.py
@@ -22,7 +22,7 @@ class ControlData:
         self.csv_path = self.__filepath + 'temp-data-' + str(dt.datetime.now()) + '.csv'
 
         self.plot_path = 'plots/report_plot-' + str(dt.date.today()) + '.png'
-        self.html_plot_path = '/' + 'plots/report_plot-' + str(dt.date.today()) + '.png'
+        self.html_plot_path = '../' + 'plots/report_plot-' + str(dt.date.today()) + '.png'
 
     def new_file(self):
         self.csv_path = self.__filepath + 'temp-data-' + str(dt.datetime.now()) + '.csv'
